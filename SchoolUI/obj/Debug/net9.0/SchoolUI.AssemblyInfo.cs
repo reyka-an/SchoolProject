0edd3c18be5a4e5ea4c10428315dbb9a0b89a3a7
Reyka <blueorange477@gmail.com>
2025-07-19 15:38:45 +0300

    add pages students

--- SchoolUI/obj/Debug/net9.0/SchoolUI.AssemblyInfo.cs
+++ SchoolUI/obj/Debug/net9.0/SchoolUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c2549c05af042af84c629620c81d1fc1c0f425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848fcba72cca8edec577c41756b5316a6ea11b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
